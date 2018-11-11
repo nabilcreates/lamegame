dc21f1e2bbe409da7057dec5e59fc5939c34a7b5
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2018-11-11 08:58:59 +0800

    Added pickups and added dist()

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 var rx, ry, rs;
+var pickups = [];
 
 function setup() {
     createCanvas(64 * 5, 64 * 5)
+
+    pickups.push({
+        x: 0,
+        y: 0,
+        s: 64,
+    })
 
     rx = 0;
     ry = 0;
@@ -9,31 +16,52 @@ function setup() {
 }
 
 function draw() {
+
+    ellipseMode(CORNER)
+    
     background(0)
     rect(rx, ry, rs, rs)
 
     boundary()
 
+    // PICKUP LENGTH ARE NOT FROM 0, SO THE LENGTH IS 1 BUT THE INDEX IS 0 SO LENGTH -1 IS INDEX
+    ellipse(pickups[pickups.length - 1].x, pickups[pickups.length - 1].y, pickups[pickups.length - 1].s)
+
+    console.log(dist(rx, ry, pickups[pickups.length - 1].x, pickups[pickups.length - 1].y, ))
+
+    if (dist(rx, ry, pickups[pickups.length - 1].x, pickups[pickups.length - 1].y, ) <= pickups[pickups.length - 1].s) {
+        // score++
+        spawn(pickups.length-1)
+    }
+
 }
 
-function boundary(){
+function spawn(index) {
+    pickups[index] = {
+        x: random(width),
+        y: random(height),
+        s: 64,
+    }
+}
 
-    if(rx > width){
+function boundary() {
+
+    if (rx > width) {
         rx = 0
     }
 
-    if(rx < 0){
+    if (rx < 0) {
         rx = width
     }
 
-    if(ry > height){
+    if (ry > height) {
         ry = 0;
     }
 
-    if(ry < 0){
+    if (ry < 0) {
         ry = height;
     }
-    
+
 }
 
 function keyPressed(event) {
